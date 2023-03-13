247b55ab91e0d6aed4141710de7e59cfe51a7b19
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-03-13 10:23:39 +0000

    fix

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ const app = express();
 app.get('/cards', (req, res) => {
   readCards()
     .then((data) => res.send(data))
-    .catch(() =>
-      res.status(500).send({ message: 'An error has occurred on the server.' })
-    );
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server.' }));
 });
 
 app.get('/users', (req, res) => {
   readUsers()
     .then((data) => res.send(data))
-    .catch(() =>
-      res.status(500).send({ message: 'An error has occurred on the server.' })
-    );
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server.' }));
 });
 
 app.listen(3000, () => console.log('Server is listening on port 3000'));
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,14 @@ const fs = require('fs');
 
 const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
-const readUsers = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(usersPath, { encoding: 'utf8' }, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(data));
-      }
-    });
+const readUsers = () => new Promise((resolve, reject) => {
+  fs.readFile(usersPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
   });
-};
+});
 
 module.exports = readUsers;
