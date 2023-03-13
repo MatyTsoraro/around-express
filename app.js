70842bc890768c6eab939d7de04e3c5fff36d26c
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-03-13 19:41:32 +0000

    fix

--- app.js
+++ app.js
@@ -16,4 +16,15 @@ app.get('/users', (req, res) => {
     .catch(() => res.status(500).send({ message: 'An error has occurred on the server.' }));
 });
 
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  readUsers(id)
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server.' }));
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Requested resource not found' });
+});
+
 app.listen(3000, () => console.log('Server is listening on port 3000'));
