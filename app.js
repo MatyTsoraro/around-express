1cb47d1b030d73d1aea05357b3cca97b17eaa7a9
MatyTsoraro <maty.tsoraro@gmail.com>
2023-03-23 23:59:31 +0200

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
 const express = require('express');
 const readCards = require('./routes/cards');
 const readUsers = require('./routes/users');
