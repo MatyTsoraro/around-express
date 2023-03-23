ff0afe275f509582fd9fd7c74ed58886b0b02e0c
MatyTsoraro <maty.tsoraro@gmail.com>
2023-03-24 00:24:08 +0200

    Creating Schemas and Models

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,35 @@ const readUsers = () => new Promise((resolve, reject) => {
   });
 });
 
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        // Regex to validate URL format
+        return /^(https?:\/\/)(www\.)?([\w\-\.]+)\.([a-z]{2,6})(\/[\w\-\.~:?#[\]@!$&'()*+,;=]*)?#?$/.test(v);
+      },
+      message: 'Invalid URL format'
+    }
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
+
 module.exports = readUsers;
