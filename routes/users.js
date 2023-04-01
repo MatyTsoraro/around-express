853c02e72fc885fec42b39c2a59a897eb91ea435
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-04-01 20:54:32 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
-
-// Middleware function to add user object to request
-const addUserToObject = (req, res, next) => {
-  req.user = {
-    _id: '642336d883cf88e0204620bd' // paste the _id of the test user created in the previous step
-  };
-  next();
-};
-
-// The following function reads all users from the database.
-const readUsers = () => {
-  return User.find({});
-};
-
-// The following function creates a new user in the database.
-const createUser = (name, about, avatar) => {
-  return User.create({ name, about, avatar });
-};
-
-module.exports = {
-  addUserToObject,
-  readUsers,
+const router = require('express').Router();
+const {
+  getUsers,
+  getUser,
   createUser,
-};
+  updateUser,
+  updateAvatar,
+} = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get('/users/:userId', getUser);
+router.post('/users', createUser);
+router.patch('/users/:userId', updateUser);
+router.patch('/users/:userId/avatar', updateAvatar);
 
 module.exports = router;
-module.exports.createUser = createUser;
