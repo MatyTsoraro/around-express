fd32ac5b9c29a9728bba62db0e13622c498eac8a
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-04-01 20:53:08 +0300

    Update card.js

--- models/card.js
+++ models/card.js
@@ -1,38 +1,40 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+const { urlRegex } = require('../utils/consts');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: {
-      validator(v) {
-        return validator.isURL(v);
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'The "name" field must be filled in.'],
+      minlength: [2, 'The minimum length of name is 2'],
+      maxlength: [30, 'The maximum length of name is 2'],
+    },
+    link: {
+      type: String,
+      required: [true, 'The "Link" field must be filled in.'],
+      validate: {
+        validator(value) {
+          return urlRegex.test(value);
+        },
+        message: 'Invalid URL',
       },
-      message: 'Invalid URL format',
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: [true, 'The "Owner" field must be filled in.'],
+    },
+    likes: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('card', cardSchema);
-
