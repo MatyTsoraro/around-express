43bfd6ebf5cb52eafdd8956d204ab3c44339e120
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-04-01 20:54:06 +0300

    Update cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -1,26 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const Card = require('../models/card');
+const router = require('express').Router();
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  unlikeCard,
+} = require('../controllers/cards');
 
-router.get('/:id', (req, res) => {
-  const cardId = req.params.id;
-
-  Card.findById(cardId)
-    .orFail(() => {
-      const error = new Error('Card not found');
-      error.statusCode = 404;
-      throw error;
-    })
-    .then((card) => {
-      res.status(200).send(card);
-    })
-    .catch((error) => {
-      if (error.statusCode) {
-        res.status(error.statusCode).send({ error: error.message });
-      } else {
-        res.status(500).send({ error: 'Internal server error' });
-      }
-    });
-});
+router.get('/cards', getCards);
+router.post('/cards', createCard);
+router.delete('/cards/:cardId', deleteCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', unlikeCard);
 
 module.exports = router;
