5e1da8d9515eff0891832a630499b87094050dc0
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-04-01 20:51:57 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,46 +1,94 @@
-// Import the Card model from your database schema
 const Card = require('../models/card');
+const { customError } = require('../utils/consts');
 
-// Define the like card controller
-module.exports.likeCard = (req, res) => {
-  // Get the card ID from the request params
-  const cardId = req.params.cardId;
-  // Get the user ID from the request object
-  const userId = req.user._id;
-  // Like a card using the Card model and the findByIdAndUpdate method with $addToSet operator
-  Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
-    .then((card) => {
-      if (!card) {
-        return res.status(404).send({ message: 'Card not found' });
-      }
-      res.send(card);
-    })
+// GET
+
+const getCards = (req, res) => {
+  Card.find({})
+    .populate('owner')
+    .then((cards) => res.status(200).send({ data: cards }))
+    .catch(() => customError(res, 500, 'We have encountered an error'));
+};
+// POST
+const createCard = (req, res) => {
+  const { name, link, likes } = req.body;
+  const { _id } = req.user;
+
+  Card.create({
+    name,
+    link,
+    likes,
+    owner: _id,
+  })
+    .then((card) => res.status(201).send({ data: card }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Invalid card ID' });
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: `${Object.values(err.errors)
+            .map((error) => error.message)
+            .join(', ')}`,
+        });
+      } else {
+        customError(res, 500, 'We have encountered an error');
       }
-      res.status(500).send({ message: 'Server error' });
     });
 };
 
-// Define the dislike card controller
-module.exports.dislikeCard = (req, res) => {
-  // Get the card ID from the request params
-  const cardId = req.params.cardId;
-  // Get the user ID from the request object
-  const userId = req.user._id;
-  // Dislike a card using the Card model and the findByIdAndUpdate method with $pull operator
-  Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
-    .then((card) => {
-      if (!card) {
-        return res.status(404).send({ message: 'Card not found' });
-      }
-      res.send(card);
+const deleteCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.deleteOne({ cardId })
+    .orFail(() => {
+      const error = new Error('no Card found for the specifed id');
+      error.statusCode = 404;
+      throw error;
     })
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Invalid card ID' });
+        customError(res, 400, 'Invaild Card ID');
+      } else if (err.statusCode === 404) {
+        customError(res, 404, err.message);
+      } else {
+        customError(res, 500, 'We have encountered an error');
       }
-      res.status(500).send({ message: 'Server error' });
     });
+};
+const updateLikes = (req, res, operator) => {
+  const { cardId } = req.params;
+  const { _id } = req.user;
+
+  Card.findByIdAndUpdate(
+    cardId, // searches for the card on the database
+    { [operator]: { likes: _id } }, // $pull / $addToSet
+    { new: true },
+  )
+    .orFail(() => {
+      const error = new Error('Card is not found');
+      error.status = 404;
+
+      throw error;
+    })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        customError(res, 400, 'Card id is incorrect');
+      } else if (err.status === 404) {
+        customError(res, 404, 'Invalid user id');
+      } else {
+        customError(res, 500, 'Something went wrong');
+      }
+    });
+};
+
+const likeCard = (req, res) => updateLikes(req, res, { $addToSet: {} });
+
+const unlikeCard = (req, res) => updateLikes(req, res, { $pull: {} });
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  unlikeCard,
 };
