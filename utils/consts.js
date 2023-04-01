adf6f05cd3c6c16425d3964e69c668fcd01a0355
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-04-01 21:12:51 +0000

    eslint fix

--- utils/consts.js
+++ utils/consts.js
@@ -1,4 +1,3 @@
-
 const urlRegex = /(http(s)?:\/\/.)?(www.)?[-a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
 const customError = (res, number, txt) => res.status(number).send({ message: txt });
